2944c1786860305d9af384f45ebf2e337de8c1c1
Mental <Mentaleak@gmail.com>
2018-11-01 12:33:43 -0400

    added PSGit.ps1

---
+++ PSGit.ps1
@@ -0,0 +1,73 @@
+function New-GitRepo(){
+    param(
+        [Parameter(mandatory=$true)][string]$token,
+        [Parameter(mandatory=$true)][string]$name
+    )
+    # https://github.com/settings/tokens/new
+# curl https://api.github.com/user/repos?access_token=$token -d '{"name":"$name"}'
+
+}
+
+$crd
+function New-GitToken() {
+     param(
+     [string]$user,
+     [string]$pass,
+     [PSCredential]$cred
+     )
+     if($cred)
+     {
+
+     }
+     elseif($user -and $pass)
+     {
+
+
+     }
+     else{
+     throw [System.IO.FileNotFoundException] "Please Provide either (-cred) or (-user and -pass)"
+     }
+
+
+$pair = "${user}:${pass}"
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+$base64 = [System.Convert]::ToBase64String($bytes)
+$basicAuthValue = "Basic $base64"
+#$headers = @{ Authorization = $basicAuthValue}
+$postparams = @{scopes = "repo";note = "ZPSMODULE" }
+#Invoke-WebRequest -uri "https://api.github.com/authorizations" -Headers $headers -Method POST -body $postparams
+return $basicAuthValue
+#curl -u '$username'  -d '{"scopes":["repo"],"note":"ZPSMODULE"}' https://api.github.com/authorizations
+#Curl -u does not exsist in powershell we will have to find a way to do this with invoke-webrequest
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$gitToken=New-GitToken
+$headers = @{ Authorization = $gitToken}
+$postparams = @{scopes = "repo";note = "ZPSMODULE" }
+$connection = Invoke-WebRequest -Uri https://api.github.com -Headers $headers
+
+
+
+
+
+# Binds folder to github
+# requires git
+function New-RepoConnection()
+{
+     param(
+     #Example: C:\Scripts\project1
+     [Parameter(mandatory=$true)][string]$LocalPath,
+     #Example: https://github.com/{USERNAME}/{REPO}
+     [Parameter(mandatory=$true)][string]$RemoteRepo
+     )
+git init
+git add .
+git commit -m "First commit"
+git remote add origin $RemoteRepo
+git remote -v
+git pull origin master
+}
+
+
